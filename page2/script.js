4271c1abd31d8099d8f37b21a5cb35b409844f31
sahilnagar <41125459+sahilnagar@users.noreply.github.com>
2024-02-07 20:39:08 +0530

    Add files via upload

--- page2/script.js
+++ page2/script.js
@@ -26,7 +26,7 @@ function renderExpenses() {
 		const expenseRow = document.createElement("tr"); 
 		expenseRow.innerHTML = ` 
 	<td>${expense.name}</td> 
-	<td>$${expense.amount}</td> 
+	<td>Rs.${expense.amount}</td> 
 	<td class="delete-btn" data-id="${i}">Delete</td> 
 	`; 
 		expenseList.appendChild(expenseRow); 
